3ac1424a17078be92f5ac0518dc4601ad7c7fc14
Alek <alekhrycaiko@gmail.com>
2016-10-16 17:22:15 -0700

    Installed driver and mocked a fake connection setup needing implementation

--- src/server.js
+++ src/server.js
@@ -2,3 +2,23 @@
  * Created by Alek Hrycaiko on 2016-10-16.
  */
 
+// Connect to db
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host : '???',
+    user : 'cpsc304admin',
+    password: 'Hicarpwatcher1',
+    database: 'cpsc304-db'
+});
+
+connection.connect();
+
+connection.query('SELECT SOME STUFF AND MAKE A QUERY'), function (err, rows, columns) {
+    if (err) {
+        throw new Error;
+    }
+    console.log('BOOM!');
+}
+
+connection.end();
