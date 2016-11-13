c656739f8e24195a5dc1fd458a8088177e644529
Aditi Chak <aditichak@hotmail.com>
2016-11-13 14:35:34 -0800

    added eid

--- src/server.js
+++ src/server.js
@@ -121,7 +121,7 @@ router.route('/ingredients/nonalcoholic')
 
 router.route('/employee/admin/staff')
     .get(function (req, res) {
-        var showBartender = "select name from bartender";
+        var showBartender = "select eid as id, name from bartender";
         endpoint(showBartender)
             .then(function (result) {
             res.json(result);
