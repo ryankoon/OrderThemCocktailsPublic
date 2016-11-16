2c220a4aa98deb2f774f69369fb11d6735129fc2
Chris Semiao <csemiao@gmail.com>
2016-11-16 14:49:18 -0800

    working on static file serving

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
  * Created by Alek Hrycaiko on 2016-10-16.
  */
 'use strict';
+
 // Connect to db
 var mysql = require('mysql');
 var pool  = mysql.createPool({
@@ -11,26 +12,33 @@ var pool  = mysql.createPool({
     password : '05b4c318',
     database : 'orderthemdrinksdb'
 });
-var Promise = require('bluebird');
 
+var Promise = require('bluebird');
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var request = require('request');
 var bodyParser = require('body-parser'); // used to read results from API calls.
 var path = require('path');
 var app = express();
+
+
 // setup bodyParser to get data from POST - maybe unnecessary remove if something else is preferred.
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 var router = express.Router();
 var port = process.env.PORT || 8080;
 
-app.use(express.static('js'));
+// setup static file serving.
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 var apiRouter = require('./api_routes');
 var uiRouter = require('./ui_routes');
+
 apiRouter.apiRouting(router, pool);
 uiRouter.uiRouting(app);
 
