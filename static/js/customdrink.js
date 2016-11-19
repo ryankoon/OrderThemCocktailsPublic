4c4958b6c9a16f60767d45581cf2d6fee262e366
Chris Semiao <csemiao@gmail.com>
2016-11-19 15:52:06 -0800

    css changes to list view to make it presentable.  basic front end done, but there may still be bugs

--- static/js/customdrink.js
+++ static/js/customdrink.js
@@ -12,7 +12,7 @@
             $('#infoModal').hide();
         });
 
-        $('.add-item-alcoholic').on('click', function () {
+        $('.add-item').on('click', function () {
             var $parent = $(this).parent().siblings('#data'),
                 name = $parent.attr('data-name'),
                 type = $parent.attr('data-type'),
@@ -26,7 +26,10 @@
     updateOrderHistoryDisplay = function (drinkObj) {
         if (ingredientNotInDrink(drinkObj.name)) {
             drinkIngredients.push(drinkObj.name);
-            $('.js-ingredients-added').append("<li class=\'list-group-item\' data-name=" + drinkObj.name + " data-price=" + drinkObj.price + ">" + drinkObj.type + drinkObj.name + drinkObj.price + "<button type='button' class='close list-remove'>&times;</button></li>");
+                $('.js-ingredients-added').append("<li class=\'list-group-item\' data-name=" + drinkObj.name +
+                    " data-price=" + drinkObj.price + ">" + "<div class=\'list-infobox\'><div class=\'list-type\'>" + drinkObj.type +
+                    "</div> <div class=\'list-name'>"+ drinkObj.name + "</div><div class=\'list-price\'>" +
+                    parseFloat(drinkObj.price).toFixed(2) + "</div></div><div class=\'list-buttonbox\'><button type='button' class='close list-remove'>&times;</button></div></li>");
 
             // unbind click listeners from the remove buttons, otherwise the older ones will have multiple listeners
             // bound to them.  There is probably a better way to do this.
@@ -128,7 +131,12 @@
                 alert(JSON.stringify(result[0]));
                 $('#infoModal-title').html(result[0].name);
                 $('#infoModal-description').html('"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."')
-                $('#infoModal-price').html(result[0].price);
+
+                if (parseFloat(result[0].price.toFixed(2)) == 0.00) {
+                    $('#infoModal-price').html("Free");
+                } else {
+                    $('#infoModal-price').html(parseFloat(result[0].price.toFixed(2)) );
+                }
                 $('#infoModal').show();
             },
             error: function (err) {
