ee6e9c4811ea362b1efda4a1836c8166c2aea823
Alek <alekhrycaiko@gmail.com>
2016-11-21 22:52:28 -0800

    Removed old/new login route and re-swapped route to original

--- src/ui_routes.js
+++ src/ui_routes.js
@@ -6,7 +6,7 @@ function uiRouting(app, hbs) {
 	/*
 	Home route for customers to input information.
 	*/
-	app.get('/home', function (req,res){
+	app.get('/', function (req,res){
 	  res.render('home');
 	});
 
@@ -315,9 +315,6 @@ function uiRouting(app, hbs) {
 	     res.send(body);
 	    }
 	  });
-	});
-	app.get('/', function (req, res){
-		res.render('login');
 	});
 }
 module.exports.uiRouting = uiRouting;
